df76fa45a8d92db9bac1278ade9368b8a046d259
joporig <joporig@gmail.com>
2020-02-21 19:50:11 +0300

    Line trash

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -2,10 +2,8 @@
   <div class="canvas-container">
       <canvas class="my-canvas" id="myCan" @click="CanvasClicked" width="600" height="600">
 
-      </canvas>
-      <button @click="DrawLine">Нарисовать линию
-
-        </button>
+      </canvas> 
+      <button @click="Change">Нарисовать линию</button> <br/>
   </div>
 </template>
 
@@ -34,44 +32,81 @@ export default {
     methods:{
         CanvasClear(){
             const ctx = this.canvas.getContext("2d");
-            ctx.fillStyle = "white";
+            ctx.fillStyle = "lightgrey";
             ctx.fillRect(0, 0, 600, 600)
             ctx.fill();
         },
         drawPoint(point){
-            if(this.Draw === "Point"){
-                const {x, y, color} = point
+            const {x, y, color} = point
+            const ctx = this.canvas.getContext("2d");
+            ctx.fillStyle = color;
+            const width = 5;
+            const height = 5;
+            ctx.fillRect(x - width / 2, y - height / 2, width, height);
+            ctx.fill();
+        },
+        drawLine(){
+                this.CanvasClear();
+                const {x, y, color} = this.P[0]
                 const ctx = this.canvas.getContext("2d");
                 ctx.fillStyle = color;
                 const width = 5;
                 const height = 5;
+                let dist = Math.sqrt(Math.pow(this.P[0].x - this.P[1].x, 2) + Math.pow(this.P[0].y - this.P[1].y, 2))
+                let Xcoff = Math.abs(this.P[0].x - this.P[1].x) / dist;
+                let Ycoff = Math.abs(this.P[0].y - this.P[1].y) / dist;
+                if(this.P[0].x > this.P[1].x){
+                    let a = this.P[0].x;
+                    this.P[0].x = this.P[1].x;
+                    this.P[1].x = a;
+                }
+                if(this.P[0].y > this.P[1].y){
+                    let a = this.P[0].y;
+                    this.P[0].y = this.P[1].y;
+                    this.P[1].y = a;
+                }
+                for(let i = 0; i < dist; i++){
+                    let PA = {
+                        x: this.P[0].x + Xcoff * i,
+                        y: this.P[0].y + Ycoff * i,
+                        color: "red"
+                    }
+                    this.points.push(PA);
+                }
                 ctx.fillRect(x - width / 2, y - height / 2, width, height);
                 ctx.fill();
+
+        },
+        CanvasClicked(event){
+            if(this.Draw == "Point"){
+                this.points.push({x:event.pageX, y: event.pageY, color: "green"})
+                this.$axios.get("http://188.225.47.187/api/canvas/setpoint.php", {
+                    params:{
+                        x: event.pageX,
+                        y: event.pageY
+                    }
+                })
             } else if(this.Draw == "Line"){
+                /*this.$axios.get("http://188.225.47.187/api/canvas/setpoint.php", {
+                    params:{
+                        x: event.pageX,
+                        y: event.pageY
+                    }
+                })*/
+                this.P.push({x: event.pageX, y: event.pageY});
                 if(this.P.length == 2){
-                    console.log(this.P);
+                    console.log("clicked")
+                    this.drawLine();
                     this.P.length = 0;
-                } else {
-                    this.P.push(point);
                 }
             }
         },
-        CanvasClicked(event){
-            this.points.push({x:event.pageX, y: event.pageY, color: "green"})
-            this.$axios.get("http://188.225.47.187/api/canvas/setpoint.php", {
-                params:{
-                    x: event.pageX,
-                    y: event.pageY
-                }
-            })
-        },
         loadPoints(){
             this.$axios.get("http://188.225.47.187/api/canvas/points.php").then((response)=>{
-                console.log(response)
                 this.points = response.data.filter((el) => el.x !== null && el.y !== null)
             })
         },
-        DrawLine(){
+        Change(){
             if(this.Draw == "Line")
                 this.Draw = "Point";
             else 
